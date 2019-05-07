616d50bf135eb50332831415229724b8658f4f9c
Aimee <baines@admins-iMac-4.local>
2019-05-07 15:14:12 +0100

    change title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Base title="Calculator!"/>
+        <Base title="Aimee's React Calculator!"/>
       </div>
     );
   }
