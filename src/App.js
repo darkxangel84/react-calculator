29205d5a51e44610a83999131c3913351be77cbe
user <user@users-iMac.local>
2019-06-09 20:13:27 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ class App extends Component {
     return (
       <div className="App">
         <Base title="Aimee's React Calculator!"/>
+       
+        <footer style={{paddingTop: '5em',textAlign: 'center', color: 'white'}}>
+          <p>To-Do List built with ReactJS, you can vist the code over on my <a style={{color: 'white', textDecoration: 'none'}} target="_blank" rel="noopener noreferrer" href="https://github.com/aimeetacchi/react-todo"> <i className="fab fa-github"></i> GitHub repo</a></p>
+
+          <small>Developed &amp; designed by <a style={{color: 'white', textDecoration: 'none'}} target="_blank" rel="noopener noreferrer" href="https://www.aimeetacchi.dev">Aimee Tacchi</a> 2019</small>
+        
+        </footer>
       </div>
     );
   }
